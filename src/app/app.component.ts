7e77af534b8f5a8899cc8200c304b3434bc313cd
joe mckinney and semhar kidane <semhar@>
2018-05-02 09:48:02 -0700

    add new to do list to github

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Task } from './model/to-do.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  currentFocus: string = 'Angular Homework';
+
+  tasks: Task[] = [
+    new Task('Finish weekend Angular homework for Epicodus course', 3),
+    new Task('Begin brainstorming possible JavaScript group projects', 2),
+    new Task('Add README file to last few Angular repos on GitHub', 2)
+  ];
+
+  priorityColor(currentTask){
+    if (currentTask.priority === 3){
+      return "bg-danger";
+    } else if (currentTask.priority === 2) {
+      return  "bg-warning";
+    } else {
+      return "bg-info";
+    }
+  }
+
+  selectedTask: null;
+
+  editTask(clickedTask) {
+    this.selectedTask = clickedTask;
+  }
+  finishedEditing() {
+    this.selectedTask = null;
+  }
 }
