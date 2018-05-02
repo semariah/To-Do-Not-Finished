aa40491c3ce5519ac03fb79d4679e440b5555515
joe mckinney and semhar kidane <joe+semhar@>
2018-05-02 16:37:03 -0700

    add another component to the to-do list app built with Angular to practice with components.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,31 +7,25 @@ import { Task } from './model/to-do.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
   currentFocus: string = 'Angular Homework';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+  selectedTask = null;
 
-  tasks: Task[] = [
+  masterTaskList: Task[] = [
     new Task('Finish weekend Angular homework for Epicodus course', 3),
     new Task('Begin brainstorming possible JavaScript group projects', 2),
     new Task('Add README file to last few Angular repos on GitHub', 2)
   ];
 
-  priorityColor(currentTask){
-    if (currentTask.priority === 3){
-      return "bg-danger";
-    } else if (currentTask.priority === 2) {
-      return  "bg-warning";
-    } else {
-      return "bg-info";
-    }
-  }
-
-  selectedTask: null;
-
   editTask(clickedTask) {
     this.selectedTask = clickedTask;
   }
+
   finishedEditing() {
     this.selectedTask = null;
   }
+
 }
